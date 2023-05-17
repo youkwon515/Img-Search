589d5294b6b61f47465b34bf0081b10edb050a08
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-17 09:50:34 +0900

    Feat: creact states

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import Header from "./header";
-import Main from './main';
+import Main from "./main";
 import Section from "./section";
-import Footer from "./footer";
+import { useState } from "react";
 function App() {
-    return (
-        <>
-            <Header />
-            <Main />
-            <Section />
-            <Footer />
-        </>
-        
-    );
+  const [Value, setValue] = useState("");
+  const [search, setSearch] = useState("");
+  const [url, setUrl] = useState("");
+  async function searchImg() {
+    const url =
+      "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=10&autoCorrect=true";
+    const options = {
+      method: "GET",
+      headers: {
+        "content-type": "application/octet-stream",
+        "X-RapidAPI-Key": "3c68bf92aemshe67110d6e3c645ap1bbb1ajsn0b8883d7964b",
+        "X-RapidAPI-Host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+      },
+    };
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      return await result.value;
+      // result.value[0].provider.name 이름
+      // result.value[0].url 이미지
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const saveValue = (event) => {
+    setValue(event.target.value);
+  };
+  const onClickBtn = async () => {
+    const searchUrl = await searchImg();
+    setUrl(searchUrl);
+    setSearch(Value);
+  };
+  return (
+    <>
+      <Header />
+      <Main onClick={onClickBtn} value={Value} onChange={saveValue} />
+      <Section search={search} url={url} />
+    </>
+  );
 }
 
 export default App;
