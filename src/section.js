93b951fba0ee4248c956361bd2db8062506001cf
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-17 09:54:09 +0900

    FEAT: 검색 결과 처리

--- src/section.js
+++ src/section.js
@@ -1,18 +1,45 @@
-import './styles.css';
-
-function Section() {
-    return (
-        <section>
-            <ul>
-                <li><div className="section-result">검색결과</div></li>
-                <li><div className="section-line"></div></li>
-                <li><img className="section-img1" src="https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/cEmV/image/waD2sHkLtNV__uKphcoyhj3f_OE.jpg" alt="사진1"/></li>
-                <li><img className="section-img2" src="https://www.hynews.ac.kr/news/photo/202009/10519_8579_1515.jpg" alt="img2"/></li>
-                <li><img className="section-img3" src="https://static.newswire.co.kr/property/img/sub/edu_pressWrite_photographing_img.jpg" alt="img3"/></li>
-                <li><div className="section-line"></div></li>
-            </ul>
-        </section>
-    );
+import "./styles.css";
+import NoImg from "./NoImg.jpg";
+function Section({ search, url }) {
+  const urls = url;
+  const onErrorImg = (e) => {
+    e.target.src = NoImg;
+  };
+  return (
+    <section>
+      <ul>
+        <li>
+          <div className="section-result">
+            {search ? `"${search}"` : ""} 검색결과
+          </div>
+        </li>
+        <li>
+          <div className="section-line"></div>
+        </li>
+        {urls &&
+          urls.map(({ url, width, height }, i) => {
+            try {
+              return (
+                <li>
+                  <img
+                    className={`section-img`}
+                    src={url}
+                    width={`${width}px`}
+                    height={`${height}px`}
+                    onError={onErrorImg}
+                  />
+                </li>
+              );
+            } catch (e) {
+              console.log("이미지 없음");
+            }
+          })}
+        <li>
+          <div className="section-line"></div>
+        </li>
+      </ul>
+    </section>
+  );
 }
 
 export default Section;
