6a88377b9efd178f2072add86c67b86e4dce54b3
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-17 09:53:34 +0900

    FEAT: 검색 기능 추가

--- src/main.js
+++ src/main.js
@@ -1,48 +1,17 @@
-import './styles.css';
-import searchIcon from './searchIcon.svg';
+import "./styles.css";
+import searchIcon from "./searchIcon.svg";
 
-function clickE() {
-    let inputValue = document.querySelector("input").value;
-    console.log(inputValue);
+function Main({ onClick, value, onChange }) {
+  return (
+    <main>
+      <span>ImgSearch</span>
+      <div>
+        <img src={searchIcon} alt="검색아이콘" />
+        <input type="text" value={value} onChange={onChange} />
+      </div>
+      <button onClick={onClick}>검색하기</button>
+    </main>
+  );
 }
-
-function Main() {
-    return (
-        <main>
-            <span>TITLE</span>
-            <div>
-                <img src={searchIcon} alt="검색아이콘"/>
-                <input type="text"/>
-            </div>
-            <button onClick={clickE}>검색하기</button>
-        </main>
-    );
-}
-
-
-
-
-// async function search() {
-//     const url = 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=10&autoCorrect=true';
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             'content-type': 'application/octet-stream',
-//             'X-RapidAPI-Key': '3c68bf92aemshe67110d6e3c645ap1bbb1ajsn0b8883d7964b',
-//             'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
-//         }
-//     };
-//     try {
-//         const response = await fetch(url, options);
-//         const result = await response.json();
-//         console.log(result.value[0].provider.name);
-//         // result.value[0].provider.name 이름
-//         // result.value[0].url 이미지
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
-
-// search();
 
 export default Main;
