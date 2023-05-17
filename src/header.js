4dd9de7218813ef76dfff09f740f64b3170c31ea
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-17 09:52:48 +0900

    FEAT: 이미지 불러오기

--- src/header.js
+++ src/header.js
@@ -1,21 +1,11 @@
-import './styles.css';
+import "./styles.css";
 
 function Header() {
-    return (
-        <header>
-            <div className="logo">LOGO</div>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="#">menu1</a>
-                    </li>
-                    <li>
-                        <a href="#">menu2</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <div className="logo">ImgSearch</div>
+    </header>
+  );
 }
 
 export default Header;
